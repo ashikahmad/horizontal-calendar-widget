c03e2e1195128ccf6cdc1c1a049b35b5c5a663ac
DenizUgur <deniz343@gmail.com>
2017-05-18 17:09:06 +0300

    Clean up
    Variables are defined on the start of the document

--- hcal.widget/index.js
+++ hcal.widget/index.js
@@ -1,14 +1,26 @@
-command: "curl -s 'https://clearoutside.com/forecast/{LAT}/{LONG}?view=midnight' 'https://www.timeanddate.com/moon/{country}/{city}'",
+/* Please check the correct values for your location to ensure you get the most reliable information.
 
-dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-offdayIndices: [0, 6], // Saturday & Sunday
- 
-refreshFrequency: 5000,
-displayedDate: null,
+   Go to these pages and confirm values from url
+    - https://clearoutside.com
+    - https://www.timeanddate.com/moon
+*/
 
-render: function () {
-  return "<style>@-webkit-keyframes pulse{0%{opacity:0}100%{opacity:1}}</style>\
+_: (() => this.options = {
+        lat: '52.01',
+        long: '11.46',
+        country: 'usa',
+        city: 'miami'
+    })(),
+
+    dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    offdayIndices: [0, 6], // Saturday & Sunday
+
+    refreshFrequency: '1d',
+    displayedDate: null,
+
+    render: function () {
+        return "<style>@-webkit-keyframes pulse{0%{opacity:0}100%{opacity:1}}</style>\
       <div class=\"cal-container\">\
   <div class=\"title\"></div>\
   <table>\
@@ -19,9 +31,9 @@ render: function () {
   <tr class=\"status_bottom\"></tr>\
   </table>\
   </div>";
-},
- 
-style: "                              \n\
+    },
+
+    style: "                              \n\
   bottom: 10px                        \n\
   right: 30px                         \n\
   left: 29.5% \n\
@@ -123,62 +135,75 @@ style: "                              \n\
    opacity: .8                        \n\
 ",
 
-update: function (output, domEl) {
-  // var date = output.split("\n"), firstWeekDay = date[0], lastDate = date[1], today = date[2], m = date[3]-1, y = date[4];
-  
-  // // DON'T MANUPULATE DOM IF NOT NEEDED
-  // if(this.displayedDate != null && this.displayedDate == output) return;
-  // else this.displayedDate = output;
+    update: function (dull, domEl) {
+        // var date = output.split("\n"), firstWeekDay = date[0], lastDate = date[1], today = date[2], m = date[3]-1, y = date[4];
 
-  var date = new Date(), y = date.getFullYear(), m = date.getMonth(), today = date.getDate();
-  
-  // DON'T MANUPULATE DOM IF NOT NEEDED
-  var newDate = [today, m, y].join("/");
-  if(this.displayedDate != null && this.displayedDate == newDate) return;
-  else this.displayedDate = newDate;
+        // // DON'T MANUPULATE DOM IF NOT NEEDED
+        // if(this.displayedDate != null && this.displayedDate == output) return;
+        // else this.displayedDate = output;
+        
+        var command = `curl -s 'https://clearoutside.com/forecast/${this._["lat"]}/${this._["long"]}?view=midnight' 'https://www.timeanddate.com/moon/${this._["country"]}/${this._["city"]}'`
 
-  var firstWeekDay = new Date(y, m, 1).getDay();
-  var lastDate = new Date(y, m + 1, 0).getDate();
-    
-  var htmlOut = $.parseHTML(output);
-  
-  var weekdays = "", midlines = "", dates = "", status_top = "", status_bottom = "";
+        this.run(command, (err, output) => {
 
-  for (var i = 1, w = firstWeekDay, d = 0; i <= lastDate; i++, w++) {
-    w %= 7;
-    var isToday = (i == today), isOffday = (this.offdayIndices.indexOf(w) != -1);
-    var className = "ordinary";
-    if(isToday && isOffday) className = "off-today";
-    else if(isToday) className = "today";
-    else if(isOffday) className = "offday";
-      
-    var moon = ""; //Time&Date Moon Phases
-    var moon_table = $(htmlOut).find("#tb-7dmn").get(0);
-    var day_array = $(moon_table).find("tbody > tr").get(i - 1);
-    var moon_status = $(day_array).find("img").attr("title");
-      if (moon_status != null) moon = moon_status.substr(0, moon_status.indexOf(" "));
-      
-    var fS = ""; //Clear Outside Forecast
-    if (i >= today && i < today + 7) {
-        var query = $(htmlOut).find(".fc_hour_ratings").get(d);
-        var table = $(query).find("ul").get(0);
-            $(table).find("li").each(function(a) {
-            if ($(this).hasClass("fc_good")) fS = "open";
+            var date = new Date(),
+                y = date.getFullYear(),
+                m = date.getMonth(),
+                today = date.getDate();
+
+            // DON'T MANUPULATE DOM IF NOT NEEDED
+            var newDate = [today, m, y].join("/");
+            if (this.displayedDate != null && this.displayedDate == newDate) return;
+            else this.displayedDate = newDate;
+
+            var firstWeekDay = new Date(y, m, 1).getDay();
+            var lastDate = new Date(y, m + 1, 0).getDate();
+
+            var htmlOut = $.parseHTML(output);
+
+            var weekdays = "",
+                midlines = "",
+                dates = "",
+                status_top = "",
+                status_bottom = "";
+
+            for (var i = 1, w = firstWeekDay, d = 0; i <= lastDate; i++, w++) {
+                w %= 7;
+                var isToday = (i == today),
+                    isOffday = (this.offdayIndices.indexOf(w) != -1);
+                var className = "ordinary";
+                if (isToday && isOffday) className = "off-today";
+                else if (isToday) className = "today";
+                else if (isOffday) className = "offday";
+
+                var moon = ""; //Time&Date Moon Phases
+                var moon_table = $(htmlOut).find("#tb-7dmn").get(0);
+                var day_array = $(moon_table).find("tbody > tr").get(i - 1);
+                var moon_status = $(day_array).find("img").attr("title");
+                if (moon_status != null) moon = moon_status.substr(0, moon_status.indexOf(" "));
+
+                var fS = ""; //Clear Outside Forecast
+                if (i >= today && i < today + 7) {
+                    var query = $(htmlOut).find(".fc_hour_ratings").get(d);
+                    var table = $(query).find("ul").get(0);
+                    $(table).find("li").each(function (a) {
+                        if ($(this).hasClass("fc_good")) fS = "open";
+                    });
+                    d++
+                }
+
+                weekdays += "<td class=\"" + className + "\">" + this.dayNames[w] + "</td>";
+                midlines += "<td class=\"" + className + "\"></td>";
+                dates += "<td class=\"" + className + "\">" + i + "</td>";
+                status_top += "<td class=\"" + fS + "\"></td>";
+                status_bottom += "<td class=\"" + moon + "\"></td>";
+            };
+
+            $(domEl).find(".title").html(this.monthNames[m] + " <span style='color:lightgray'>" + y + "</span>");
+            $(domEl).find(".weekday").html(weekdays);
+            $(domEl).find(".midline").html(midlines);
+            $(domEl).find(".date").html(dates);
+            $(domEl).find(".status_top").html(status_top);
+            $(domEl).find(".status_bottom").html(status_bottom);
         });
-        d++
     }
-
-    weekdays += "<td class=\""+className+"\">" + this.dayNames[w] +"</td>";
-    midlines += "<td class=\""+className+"\"></td>";
-    dates += "<td class=\""+className+"\">" + i + "</td>";
-    status_top += "<td class=\""+fS+"\"></td>";
-    status_bottom += "<td class=\""+moon+"\"></td>";
-  };
-
-  $(domEl).find(".title").html(this.monthNames[m]+" <span style='color:lightgray'>"+y+"</span>");
-  $(domEl).find(".weekday").html(weekdays);
-  $(domEl).find(".midline").html(midlines);
-  $(domEl).find(".date").html(dates);
-  $(domEl).find(".status_top").html(status_top);
-  $(domEl).find(".status_bottom").html(status_bottom);
-}
